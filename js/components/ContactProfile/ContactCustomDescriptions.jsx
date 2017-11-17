e38233662ede0cd982f7fbcdaeed50d9e0c71471
Julie Pan <ycp217@nyu.edu>
2017-11-18 07:49:53 +0800

    add editing to customfields

--- js/components/ContactProfile/ContactCustomDescriptions.jsx
+++ js/components/ContactProfile/ContactCustomDescriptions.jsx
@@ -10,12 +10,13 @@ const ContactCustomDescriptions = ({contact, patchContact, list, listsBelong}) =
     flatten(listsBelong.map(list => list.fieldsmap)).filter(listFieldObj => listFieldObj.customfield),
     listFieldObj => listFieldObj.value
     );
-  console.log(allCustomListFields);
+  // console.log(allCustomListFields);
   const customfieldsWithLabel = contact.customfields.map((field) => {
     const listFieldWithLabel = find(allCustomListFields, listFieldObj => listFieldObj.value === field.name);
     if (!listFieldWithLabel) return undefined;
     return Object.assign({}, field, {label: listFieldWithLabel.name});
   }).filter(obj => !!obj);
+  // console.log(contact);
   return (
     <div id='contact_profile_custom_hop' style={{marginTop: 10, marginBottom: 20, marginLeft: 8}}>
       <span style={{fontSize: '1.1em'}}>Custom Fields</span>
@@ -28,39 +29,15 @@ const ContactCustomDescriptions = ({contact, patchContact, list, listsBelong}) =
           disabled={contact.readonly}
           content={customfield.value}
           contentTitle={customfield.label}
+          onBlur={(value) => {
+            const newCustomFields = contact.customfields.map(f => f.name === customfield.name ? Object.assign({}, f, {value}) : f);
+            // console.log(newCustomFields);
+            patchContact(contact.id, {
+              customfields: newCustomFields
+            });
+          }}
           />
           ) : null}
-      {/*
-        list && contact && list.fieldsmap.some(fieldObj => fieldObj.customfield && !fieldObj.readonly) ?
-        list.fieldsmap
-        .filter(fieldObj => fieldObj.customfield && !fieldObj.readonly)
-        .map((fieldObj, i) => {
-          const customValue = find(contact.customfields, customObj => customObj.name === fieldObj.value);
-          return (
-            <ContactDescriptor
-            disabled={contact.readonly}
-            key={i}
-            showTitle
-            content={customValue && customValue.value}
-            contentTitle={fieldObj.name}
-            onBlur={(value) => {
-              let customfields;
-              if (contact.customfields.length === 0) {
-                customfields = [{name: fieldObj.value, value}];
-              } else if (!contact.customfields.some(customObj => customObj.name === fieldObj.value)) {
-                customfields = [...contact.customfields.filter(field => !list.fieldsmap.some(obj => obj.readonly && obj.value === field.name)), {name: fieldObj.value, value}];
-              } else {
-                customfields = contact.customfields.map(customObj => {
-                  if (customObj.name === fieldObj.value) return {name: fieldObj.value, value};
-                  return customObj;
-                })
-                .filter(field => !list.fieldsmap.some(obj => obj.readonly && obj.value === field.name));
-              }
-              patchContact(contact.id, {customfields});
-            }}
-            />);
-        }) : <span style={{fontSize: '0.9em', color: grey700}}>There are no custom fields. You can generate them as custom columns in Sheet.</span>
-      */}
       </div>
     </div>);
 };
